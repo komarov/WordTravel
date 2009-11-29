dde089f7e2349732acd0920882c8fcc3c15ab5b0
Komarov Oleg <komarov.om@gmail.com>
2009-11-29 20:33:16 +0300

    simple interface module

--- lib/WordGraph.pm
+++ lib/WordGraph.pm
@@ -1,5 +1,28 @@
 use MooseX::Declare;
 use WordGraph::Types;
+use WordGraph::Frame;
+use WordGraph::User;
+
 
 class WordGraph {
+   #-------------------------------------------------------------------------------
+   method getUser( Maybe[Data::GUID] $Uid? ) {
+      if( $Uid ) {
+         return WordGraph::User->new( Uid => $Uid );
+      }
+      else {
+         return WordGraph::User->new();
+      }
+   }
+
+
+   #-------------------------------------------------------------------------------
+   method getFrame( Maybe[Data::GUID] $Uid? ) {
+      if( $Uid ) {
+         return WordGraph::Frame->new( Uid => $Uid );
+      }
+      else {
+         return WordGraph::Frame->new();
+      }
+   }
 }
